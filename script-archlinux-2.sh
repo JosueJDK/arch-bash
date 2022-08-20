f4d28e91a1ec67fd67f8f6f77b0c4283ae5089dd
Jou Code <salazarjosue@gmail.com>
2022-08-20 16:10:15 -0500

    Add sleep 5

--- script-archlinux-2.sh
+++ script-archlinux-2.sh
@@ -1,36 +1,63 @@
 echo "Seleccion zona horaria:"
+sleep 5
 ln -sf /usr/share/zoneinfo/America/Panama /etc/localtime
+sleep 5
 locale-gen
+sleep 5
 
 echo "Agregando lenguaje: "
+sleep 5
+
 echo LANG=es_PE.UTF-8 > /etc/locale.conf
+sleep 5
 cat /etc/locale.conf
+sleep 5
 
 echo "Agregando distribucion de teclado: "
+sleep 5
 echo KEYMAP=us > /etc/vconsole.conf
+sleep 5
 cat /etc/vconsole.conf
+sleep 5
 
 echo "Configurando RED: "
+sleep 5
 echo arch-pc > /etc/hostname
+sleep 5
 cat /etc/hostname
+sleep 5
 
 echo -e "# Static table lookup for hostnames.\n# See hosts(5) for details.\n\n127.0.0.1      localhost\n::1            localhost" > /etc/hosts
+sleep 5
 cat /etc/hosts
+sleep 5
 
 echo "Introduce la password ROOT:"
+sleep 5
 passwd
+sleep 5
 
 echo "Busque #%wheel ALL=(ALL) ALL y descomentelo: "
+sleep 5
 nano /etc/sudoers
+sleep 5
 
 echo "Introduce tu nombre de usuario: "
+sleep 5
 read USER_NAME
 useradd -mG wheel $USER_NAME
+sleep 5
 
 echo "Introduce tu contraseña: "
+sleep 5
 passwd $USER_NAME
+sleep 5
 
 echo "Introduce tu contraseña Configurando grub: "
+sleep 5
 pacman -S grub os-prober efibootmgr
+sleep 5
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=grub
+sleep 5
 grub-mkconfig -o /boot/grub/grub.cfg
+sleep 5
--- script-archlinux-3.sh
+++ script-archlinux-3.sh
@@ -1,23 +1,38 @@
 echo "Activar red: "
+sleep 5
 systemctl enable NetworkManager
 systemctl start NetworkManager
+sleep 5
 
 echo "Instalar dependencias para entorno de escritorio: "
+sleep 5
 pacman -S xorg-server xorg-apps xdg-user-dirs lightdm lightdm-gtk-greeter git python-pip git alacritty firefox rofi
+sleep 5
 xdg-user-dirs-update
+sleep 5
 
 echo "Instalando yay"
+sleep 5
 git clone https://aur.archlinux.org/yay.git
+sleep 5
 
 cd yay
+sleep 5
 makepkg -sri
+sleep 5
 
 echo "Instalando Cambiar entorno de lightdm: "
+sleep 5
 yay lightdm
+sleep 5
 
 echo "Instalando qtile: "
+sleep 5
 sudo pacman -S qtile pacman-contrib
+sleep 5
 sudo pip install psutil
+sleep 5
 
 systemctl enable lightdm
+sleep 5
 systemctl start lightdm
